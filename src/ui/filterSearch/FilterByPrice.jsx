47977885c63f4d08482b213e3018053119551a86
Sherzod Kutbidinov <skutbidinov1@gmail.com>
2024-05-03 16:14:13 +0200

    changed base url

--- src/ui/filterSearch/FilterByPrice.jsx
+++ src/ui/filterSearch/FilterByPrice.jsx
@@ -5,9 +5,21 @@ function FilterByPrice({ filterByMin, filterByMax, priceFrom, priceTo }) {
   return (
     <label className={styles.price_container}>
       <p className={styles.price}>Price</p>
-      <input className={styles.input} placeholder="from" type="number" value={priceFrom} onChange={filterByMin} />
+      <input
+        className={styles.input}
+        placeholder="from"
+        type="number"
+        value={priceFrom}
+        onChange={filterByMin}
+      />
 
-      <input className={styles.input} placeholder="to" type="number" value={priceTo} onChange={filterByMax} />
+      <input
+        className={styles.input}
+        placeholder="to"
+        type="number"
+        value={priceTo}
+        onChange={filterByMax}
+      />
     </label>
   );
 }
